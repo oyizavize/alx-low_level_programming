6ec126861361a5db6585240335801a49e291a557
oyizavize <ihiovip1@gmail.com>
2022-06-02 16:13:48 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,4 @@
-#include <stdio.h> 
-#include <unistd.h>
-                                                                                    
+#include <stdio.h>                                                                   
 int main(void) {write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);                                                                             
 }
