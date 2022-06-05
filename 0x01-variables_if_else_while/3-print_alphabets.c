463e9993bf0bf78e3a472a717e232973d81ddf5d
oyizavize <ihiovip1@gmail.com>
2022-06-05 09:42:03 -0700

    correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase, and then in uppercase.
- *
+ * main - Prints the alphabet in lowercase, and then in uppercase. *
  * Return: Always 0.
  */
 
