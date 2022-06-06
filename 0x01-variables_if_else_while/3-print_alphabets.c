2db11c8a3ebb909ecfed487b68213d4a008ff34d
oyizavize <ihiovip1@gmail.com>
2022-06-06 11:07:19 -0700

    task 3 correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,18 +5,14 @@
  * Return: Always 0.
  */
 
-int main()
+int main(void)
 {
 	char letter;
 	
 	for (letter = 'a'; letter <= 'z'; letter++)
 	putchar(letter);
-
 	for (letter = 'A'; letter <= 'Z'; letter++
 	putchar(letter);
-
 	putchar('\n');
-
 	return (0);
 }
-
